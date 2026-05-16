c088593e14df4bd0b88cdd875e1eb6802a9d7dff
To-Ev <aremuakinteye@gmail.com>
2026-05-16 12:26:26 +0100

    Changes to be committed: 	modified:   controllers/admin/orderAdminController.js 	modified:   routes/adminOrderRoutes.js

--- controllers/admin/orderAdminController.js
+++ controllers/admin/orderAdminController.js
@@ -3,6 +3,9 @@ const Order = require("../../model/Order");
 const getAllOrders = async (req, res) => {  
     try {
         const orders = await Order.find().populate('user', 'name email');
+        if(orders.length === 0) {
+            return res.status(404).json({ err: "No orders found" });
+        };
         res.json(orders);
     }
     catch (err) {
@@ -11,4 +14,42 @@ const getAllOrders = async (req, res) => {
     }   
 };
 
-module.exports = { getAllOrders };
+const updateOrderStatus = async (req, res) => {
+    const { id } = req.params;
+    const { status } = req.body;
+    try {
+        const order = await Order.findById(id);
+
+        if (!order) {
+            return res.status(404).json({ err: "Order not found" });
+        }  
+        order.status = status || order.status;
+        order.isDelivered = 
+            status === "Delivered" ? true : order.isDelivered;
+        order.deliveredAt = 
+            status === "Delivered" ? Date.now() : order.deliveredAt;
+
+        const updatedOrder = await order.save();
+        res.json({ msg: "Order status updated", order: updatedOrder });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: "Server error" });
+    }
+};
+
+const deleteOrder = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const order = await Order.findById(id);
+        if (!order) {
+            return res.status(404).json({ err: "Order not found" });
+        }
+        await Order.findByIdAndDelete(id);
+        res.json({ msg: "Order deleted" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: "Server error" });
+    }
+};
+
+module.exports = { getAllOrders, updateOrderStatus, deleteOrder };
--- routes/adminOrderRoutes.js
+++ routes/adminOrderRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const { verifyJWT, Admin } = require("../middleware/verifyJWT");
-const { getAllOrders } = require("../controllers/admin/orderAdminController");
+const { getAllOrders, updateOrderStatus, deleteOrder } = require("../controllers/admin/orderAdminController");
 
 const router = express.Router();
 
 // @desc get all orders
 // @access private/admin
 router.get("/", verifyJWT, Admin, getAllOrders);
+
+// @desc update order status
+// @access private/admin
+router.put("/:id", verifyJWT, Admin, updateOrderStatus);
+
+// @desc delete order
+// @access private/admin
+router.delete("/:id", verifyJWT, Admin, deleteOrder);
 
 module.exports = router;
